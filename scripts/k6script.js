461f3d41c5971a7d56bc75002e58e1cd97b1adb3
scalabrinig <scalabrinig@gmail.com>
2026-04-27 23:57:21 -0300

    Commit to create scripts

---
+++ scripts/k6script.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    teste: {
+      executor: 'ramping-vus',
+      startVUs: 3,
+      stages: [
+        { target: 20, duration: '30s' },
+        { target: 100, duration: '0' },
+        { target: 100, duration: '1m' },
+	{ target: 500, duration: '0' },
+        { target: 500, duration: '1m' },
+	{ target: 1000, duration: '0' },
+        { target: 1000, duration: '1m' },
+      ],
+    },
+  },
+};
+
+export default function () {
+  http.get('http://<IP>:<PORTA>');
+  sleep(1);
+}
